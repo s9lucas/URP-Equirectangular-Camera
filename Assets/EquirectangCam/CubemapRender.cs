25f73a758ac230767df3ed8c1d2bd7d12f8c4bb2
sll0052 <stephen.lucas@unt.edu>
2020-05-07 14:37:50 -0500

    initial commit

---
+++ Assets/EquirectangCam/CubemapRender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubemapRender : MonoBehaviour
+{
+    public RenderTexture cubeMap;
+    public int faceMask = 63;
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cubeMap != null)
+            cam.RenderToCubemap(cubeMap, faceMask);
+    }
+}
